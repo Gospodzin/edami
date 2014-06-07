fe6dce84f5a983f3ad6ee304cd8cbc0e9770c2c0
Floydian888 <michal.m.jungiewicz@gmail.com>
2014-06-07 17:17:12 +0200

    Brute force calc of eps neighborhood

--- makefile-project/TestsGeneral.h
+++ makefile-project/TestsGeneral.h
@@ -1,6 +1,7 @@
 #include "Point.h"
 #include "Helpers.h"
 #include "Distances.h"
+#include "EpsNeighborhoodDetermination.h"
 
 BOOST_AUTO_TEST_SUITE (Distances)
 
@@ -15,6 +16,20 @@ BOOST_AUTO_TEST_CASE(euclidean)
 	double dist = euclideanDistance(p1,p2);
 
 	BOOST_CHECK_CLOSE(dist, sqrt(3), 0.0001);
+}
+
+BOOST_AUTO_TEST_CASE(bruteForceEpsNeighborCalc)
+{
+	Point thePoint({0.0,0.0});
+	vector<Point> otherPoints = {Point({1.0,1.0}), Point({1.9,0.0}), Point({1.0,0.0}),
+			Point({-1.5,-1.7}), Point({-3.0, 0})};
+
+	auto epsNeighborhood = bruteForceEpsNeighborhoodCalc(2.0, thePoint, otherPoints, euclideanDistance);
+
+	BOOST_CHECK(epsNeighborhood.getThePoint() == Point({0.0,0.0}));
+	for (int i = 0; i < epsNeighborhood.getNeighbors().size(); ++i) {
+		BOOST_CHECK(epsNeighborhood.getNeighbors()[i] == otherPoints[i]);
+	}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
